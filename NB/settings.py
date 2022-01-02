d784d74207a79b92c2a4a91816921a4698490dad
ritik <ritikraghuwanshi160701@gmail.com>
2022-01-03 00:51:49 +0530

    static root change

--- NB/settings.py
+++ NB/settings.py
@@ -152,6 +152,8 @@ USE_TZ = True
 
 STATIC_DIR =  os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Managing media
 MEDIA_URL = '/media/'
