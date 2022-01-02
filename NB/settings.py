e73e88fa4a2b86d891c4cea90739096463af89eb
ritik <ritikraghuwanshi160701@gmail.com>
2022-01-03 00:41:37 +0530

    tweaked some files

--- NB/settings.py
+++ NB/settings.py
@@ -49,12 +49,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'NB.urls'
@@ -147,6 +149,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
 STATIC_DIR =  os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
 
